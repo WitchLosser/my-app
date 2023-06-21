74f976367fa35d829f5e51dcb6ad2e948914b6cf
WitchLosser <anton.kozak230@gmail.com>
2023-06-21 18:36:37 +0300

    Add CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import CategoryListPage from "./compnents/category/list/CategoryListPage";
+import { Routes, Route } from "react-router-dom";
+import CategoryCreatePage from "./compnents/category/create/CategoryCreatePage";
+import CategoryEditPage from "./compnents/category/edit/CetegoryEditPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<CategoryListPage />} />
+          <Route path="category/create" element={<CategoryCreatePage />} />
+          <Route path="category/edit/:id" element={<CategoryEditPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
